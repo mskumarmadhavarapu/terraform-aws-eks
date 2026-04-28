ff068997c9e6c178a2c5e0b9c958a8cb13cda00c
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-04-28 21:16:09 +0530

    dockerfiles

---
+++ 20-sg-rules/main.tf
@@ -0,0 +1,131 @@
+resource "aws_security_group_rule" "bastion_internet" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = [local.my_ip]
+  security_group_id = local.bastion_sg_id
+}
+
+resource "aws_security_group_rule" "mongodb_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.mongodb_sg_id
+}
+
+resource "aws_security_group_rule" "redis_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.redis_sg_id
+}
+
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.mysql_sg_id
+}
+
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.rabbitmq_sg_id
+}
+
+resource "aws_security_group_rule" "ingress_alb_public" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  # Where traffic is coming from
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = local.ingress_alb_sg_id
+}
+
+# OpenVpn 
+resource "aws_security_group_rule" "openvpn_public_443" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  # Where traffic is coming from
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = local.openvpn_sg_id
+}
+
+# Admin UI
+resource "aws_security_group_rule" "openvpn_public_943" {
+  type              = "ingress"
+  from_port         = 943
+  to_port           = 943
+  protocol          = "tcp"
+  # Where traffic is coming from
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = local.openvpn_sg_id
+}
+
+resource "aws_security_group_rule" "eks_control_plane_bastion" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.eks_control_plane_sg_id
+}
+
+resource "aws_security_group_rule" "eks_node_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.eks_node_sg_id
+}
+
+resource "aws_security_group_rule" "eks_control_plane_eks_node" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "-1" # all traffic
+  # Where traffic is coming from
+  source_security_group_id = local.eks_node_sg_id
+  security_group_id = local.eks_control_plane_sg_id
+}
+
+resource "aws_security_group_rule" "eks_node_eks_control_plane" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1" # all traffic
+  # Where traffic is coming from
+  source_security_group_id = local.eks_control_plane_sg_id
+  security_group_id = local.eks_node_sg_id
+}
+
+resource "aws_security_group_rule" "eks_node_vpc_cidr" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1" # all traffic
+  # Where traffic is coming from
+  cidr_blocks = ["0.0.0.0/16"]
+  security_group_id = local.eks_node_sg_id
+}
