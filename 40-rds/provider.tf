f9df396194c2d3b6436f4a8e74f8ea2ff66606a2
mskumramdhavarapu <mskumarmadhavarapu@gmail.com>
2026-04-30 21:26:24 +0530

    dockerfiles

---
+++ 40-rds/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.33.0" # Terraform AWS provider version
+    }
+  }
+ backend "s3" {
+    bucket   = "remote-state-mskdaws88s" # Replace with your bucket name
+    key      = "roboshop-eks-rds"
+    region   = "us-east-1" # Replace with your desired region
+    encrypt  = true                               # Encrypt state file at rest
+    use_lockfile  = true
+  }
+  
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
